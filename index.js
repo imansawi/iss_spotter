eafa1357fbede812ccd4fb81c6712799b3035038
imansawi <imansawi@hotmail.com>
2021-02-12 04:02:42 +0000

    Created a function calling nextISSTimesForMyLocation

--- index.js
+++ index.js
@@ -1,30 +1,10 @@
-const { fetchMyIP } = require('./iss');
-const { fetchCoordsByIP } = require('./iss');
-const { fetchISSFlyOverTimes } = require('./iss');
+const { nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-  console.log('It worked! Returned IP:' , ip);
-  console.log("================================================");
-});
 
-fetchCoordsByIP('162.245.144.188',(error, coordinates) => {
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!" , error);
-    return;
+    return console.log("It didn't work!", error);
   }
-  console.log('It worked! Returned coordinates:' , coordinates);
-  console.log("================================================");
-});
-
-fetchISSFlyOverTimes({latitude: 70, longitude: 100}, (error, passTimes) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
-  console.log('It worked! Returned flyover times:\n' , passTimes);
+  console.log(passTimes);
   console.log("================================================");
 });
