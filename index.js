ece681e83ef6f34224e3432cc839bc26194e9e07
imansawi <imansawi@hotmail.com>
2021-02-12 03:18:26 +0000

    Created a function fetches flyover times for ISS

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 fetchMyIP((error, ip) => {
   if (error) {
@@ -16,5 +17,14 @@ fetchCoordsByIP('162.245.144.188',(error, coordinates) => {
     return;
   }
   console.log('It worked! Returned coordinates:' , coordinates);
+  console.log("================================================");
+});
+
+fetchISSFlyOverTimes({latitude: 70, longitude: 100}, (error, passTimes) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned flyover times:\n' , passTimes);
   console.log("================================================");
 });
